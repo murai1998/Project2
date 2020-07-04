da9255c79f50c0c4862395ae23db7d9f772da53a
Hanna Murai <annmuray75@gmail.com>
2020-07-04 15:52:29 -0500

    weather

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Form from "./Form";
+import Weather from "./Weather";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = "31cd2a867c978a67956c6a09c99c0c25";
+class App extends Component {
+  state = {
+    temperature: "",
+    city: "",
+    country: "",
+    humidity: "",
+    description: "",
+    error: undefined
+  };
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`
+    );
+    const data = await api_call.json();
+    console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    });
+  };
+  render() {
+    return (
+      <div>
+        <h1>Current Weather</h1>
+        <Form getWeather={this.getWeather} />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Weather.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Weather = props => {
+  return (
+    <div>
+      <div>
+        {props.city && props.country && (
+          <p>
+            Location: {props.city}, {props.country},
+          </p>
+        )}
+        {props.temperature && <p>Temperature: {props.temperature},</p>}
+
+        {props.humidity && <p>Humidity: {props.humidity},</p>}
+        {props.description && <p>Description: {props.description}</p>}
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
