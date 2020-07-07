74dc7d859b4adb11e5d07e59245318ecf700f91d
ajhdrummer <thedrew88@gmail.com>
2020-07-07 14:59:04 -0400

    Andrews changes to activitie.js

--- src/Components/Activities.js
+++ src/Components/Activities.js
@@ -1,7 +1,36 @@
 import React, { Component } from "react";
+import axios from 'axios'
+
+const yelpApiKey 
+
 
 class Activities extends Component {
+
+	getYelp = async () => { 		
+		axios({
+		    method:"GET",
+		    url:`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=${'Fort Lauderdale'}`,
+		    headers:{
+		    Authorization: `Bearer ${yelpApiKey}`,
+		    },
+		    params: {
+			  categories: 'animalshelters',
+			}
+		    })
+		    .then((response)=>{
+		      console.log(response)
+		    })
+		    .catch((error)=>{
+		      console.log(error)
+		    })
+	}
+
+	componentDidMount(){
+		this.getYelp()
+	}
+
   render() {
+
     return (
       <div>
         <h1>Activities</h1>
