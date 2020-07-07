0a6b0476c64e946f91dcfd871327000ae07a24c5
Hanna Murai <annmuray75@gmail.com>
2020-07-07 09:43:51 -0500

    hotels-start

--- src/Components/Hotels.js
+++ src/Components/Hotels.js
@@ -1,7 +1,42 @@
 import React, { Component } from "react";
+import axios from "axios";
+let Amadeus = require("amadeus");
+var amadeus = new Amadeus({
+  clientId: "S0FAxRuVD6DgoEunfDeO9Brh1YRlLRBA",
+  clientSecret: "y3GnGD41wD0U9hbu"
+});
 
 class Hotels extends Component {
+  state = {
+    country: this.props.match.params.country,
+    city: this.props.match.params.city,
+    cityCode: ""
+  };
+
   render() {
+    let res = amadeus.referenceData.locations
+      .get({
+        keyword: this.state.city,
+        subType: "CITY"
+      })
+      .then(function(response) {
+        //console.log(response.data[0].iataCode);
+        return response.data[0].iataCode;
+      })
+      .catch(function(response) {
+        console.error(response);
+      });
+    console.log(res);
+    /*amadeus.shopping.hotelOffers
+      .get({
+        cityCode: res.data[0].iataCode
+      })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(response) {
+        console.error(response);
+      });*/
     return (
       <div>
         <h1>Hotels</h1>
