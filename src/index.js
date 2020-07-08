2e8a79d68cb083a23ec1cb9882e0ce6cda96b837
Renzo Pederzoli <rdp73@miami.edu>
2020-07-08 12:23:51 -0400

    flights changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
-import TestApp from "./Test-App";
+// import TestApp from "./Test-App";
 
 ReactDOM.render(
   <React.StrictMode>
