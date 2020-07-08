4ea4a97a6abe2a4ec79eefc45a2e6a5b09228070
Hanna Murai <annmuray75@gmail.com>
2020-07-08 17:47:18 -0500

    changes

--- src/Components/Activities.js
+++ src/Components/Activities.js
@@ -1,144 +1,161 @@
 import React, { Component } from "react";
-import axios from 'axios'
+import axios from "axios";
 
-
-const emptyStar = '☆'
-const fullStar = '★'
-const yelpApiKey = process.env.REACT_APP_YELP_KEY
+const emptyStar = "☆";
+const fullStar = "★";
+const yelpApiKey =
+  "cKUfNOWc1agwivRyXc65sO__wg43p0vQgstYxDXXSS-ibpb58tpc_q2JdqnBTAFcugBmH9CJhfpql4fM7jR2pbSV_FbK0Rd_tslzarrHlTCpPFBgKjb0aJDJe28EX3Yx";
 
 class Activities extends Component {
+  state = {
+    currentYelpRestaurants: {},
+    currentYelpShopping: {},
+    currentYelpMisc: {}
+  };
 
-	state = {
-		currentYelpRestaurants: {},
-		currentYelpShopping: {},
-		currentYelpMisc: {}
-	}
+  fillRate = rating => {
+    let rate = "";
+    let j = Math.floor(rating);
+    let i = 0;
+    while (i < 5) {
+      if (j > 0) {
+        rate += fullStar;
+        i++;
+        j--;
+      }
+      if (j === 0) {
+        if (i < 5) {
+          rate += emptyStar;
+          i++;
+        }
+      }
+    }
+    return rate;
+  };
 
-	fillRate = (rating) => {
-		let rate = ''
-		let j = Math.floor(rating)
-		let i = 0
-		while(i<5){
-			if(j>0){
-				rate+=fullStar
-				i++
-				j--
-			}
-			if(j===0){
-				if(i<5){
-				rate+=emptyStar
-				i++
-				}
-			}
-		}
-	return rate
-	}
+  displayCategories = business => {
+    let categoryTitles = "";
+    for (let category of business.categories) {
+      categoryTitles += category.title + ", ";
+    }
+    return categoryTitles.substring(0, categoryTitles.length - 2);
+  };
 
-	displayCategories = business => {
-		let categoryTitles=''
-		for(let category of business.categories){
-			categoryTitles += category.title + ', '
-			}
-			return categoryTitles.substring(0, categoryTitles.length - 2)	
-	}
+  displayBussinesses = bus => {
+    return bus?.businesses.map(business => {
+      return (
+        <div
+          style={{
+            border: "1px solid black",
+            width: "218px",
+            height: "414px",
+            margin: "5px"
+          }}
+        >
+          <img
+            style={{ width: "218px", height: "218px" }}
+            src={business.image_url}
+          />
+          <h2>{business.name}</h2>
+          <span>
+            {this.fillRate(business.rating)} {business.review_count} Reviews
+          </span>
+          <br />
+          <br />
+          <span>
+            <strong>{business.price}</strong> {this.displayCategories(business)}
+          </span>
+        </div>
+      );
+    });
+  };
 
-	displayBussinesses = (bus) =>{
-		return bus?.businesses.map((business)=>{
-			return(
-			 <div style={{border:'1px solid black', width:'218px', height:'414px', margin:'5px'}}>
-			 <img style={{width:'218px', height:'218px'}} src={business.image_url}/>
-			 <h2>{business.name}</h2>
-			 <span>{this.fillRate(business.rating)} {business.review_count} Reviews</span>
-			 <br/>
-			 <br/>
-			 <span><strong>{business.price}</strong> {this.displayCategories(business)}</span>
-			 </div>
-			 )
-		})
-	}
+  getYelpRestaurants = async () => {
+    axios({
+      method: "GET",
+      url: `${"https://cors-anywhere.herokuapp.com/"}https://api.yelp.com/v3/businesses/search?location=${
+        this.props.match.params.city
+      }`,
+      headers: {
+        Authorization: `Bearer ${yelpApiKey}`
+      },
+      params: {
+        categories: "restaurants",
+        limit: 5
+      }
+    })
+      .then(response => {
+        this.setState({
+          currentYelpRestaurants: response
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
-	getYelpRestaurants = async () => { 		
-		axios({
-		    method:"GET",
-		    url:`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=${this.props.match.params.city}`,
-		    headers:{
-		    Authorization: `Bearer ${yelpApiKey}`,
-		    },
-		    params: {
-			  categories: 'restaurants',
-			  limit:5
-			}
-		    })
-		    .then((response)=>{
-		      this.setState({
-		      	currentYelpRestaurants:response
-		      })
-		    })
-		    .catch((error)=>{
-		      console.log(error)
-		    })
-	}
+  getYelpShopping = async () => {
+    axios({
+      method: "GET",
+      url: `${"https://cors-anywhere.herokuapp.com/"}https://api.yelp.com/v3/businesses/search?location=${
+        this.props.match.params.city
+      }`,
+      headers: {
+        Authorization: `Bearer ${yelpApiKey}`
+      },
+      params: {
+        categories: "shopping",
+        limit: 5
+      }
+    })
+      .then(response => {
+        this.setState({
+          currentYelpShopping: response
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
-	getYelpShopping = async () => { 		
-		axios({
-		    method:"GET",
-		    url:`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=${this.props.match.params.city}`,
-		    headers:{
-		    Authorization: `Bearer ${yelpApiKey}`,
-		    },
-		    params: {
-			  categories: 'shopping',
-			  limit: 5
-			}
-		    })
-		    .then((response)=>{
-		      this.setState({
-		      	currentYelpShopping:response
-		      })
-		    })
-		    .catch((error)=>{
-		      console.log(error)
-		    })
-	}
+  getYelpMisc = async () => {
+    axios({
+      method: "GET",
+      url: `${"https://cors-anywhere.herokuapp.com/"}https://api.yelp.com/v3/businesses/search?location=${
+        this.props.match.params.city
+      }`,
+      headers: {
+        Authorization: `Bearer ${yelpApiKey}`
+      },
+      params: {
+        categories: "active"
+      }
+    })
+      .then(response => {
+        this.setState({
+          currentYelpMisc: response
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
-	getYelpMisc = async () => { 		
-		axios({
-		    method:"GET",
-		    url:`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=${this.props.match.params.city}`,
-		    headers:{
-		    Authorization: `Bearer ${yelpApiKey}`,
-		    },
-		    params: {
-			  categories: 'active',
-			  
-			}
-		    })
-		    .then((response)=>{
-		      this.setState({
-		      	currentYelpMisc:response
-		      })
-		    })
-		    .catch((error)=>{
-		      console.log(error)
-		    })
-	}
-
-	componentDidMount(){
-		this.getYelpRestaurants()
-		this.getYelpShopping()
-		this.getYelpMisc()
-	}
+  componentDidMount() {
+    this.getYelpRestaurants();
+    this.getYelpShopping();
+    this.getYelpMisc();
+  }
 
   render() {
-	console.log(this.state.currentYelpRestaurants.data?.businesses)
+    console.log(this.state.currentYelpRestaurants.data?.businesses);
     return (
-
       <div>
         <h1>Activities</h1>
-        <div style={{display:'flex', flexWrap:'wrap'}}>
-        {this.displayBussinesses(this.state.currentYelpRestaurants.data)}
-        {this.displayBussinesses(this.state.currentYelpShopping.data)}
-        {this.displayBussinesses(this.state.currentYelpMisc.data)}
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          {this.displayBussinesses(this.state.currentYelpRestaurants.data)}
+          {this.displayBussinesses(this.state.currentYelpShopping.data)}
+          {this.displayBussinesses(this.state.currentYelpMisc.data)}
         </div>
       </div>
     );
--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -16,24 +16,66 @@ class LandingPage extends Component {
   render() {
     return (
       <div>
-        <h1>My Travel Guide</h1>
-        <form>
-          <input
-            onChange={this.handleChange}
-            type="text"
-            name="city"
-            placeholder="City"
-          />
-          <input
-            onChange={this.handleChange}
-            type="text"
-            name="country"
-            placeholder="Country"
-          />
-          <Link to={`/home/${this.state.country}/${this.state.city}`}>
-            <button>Start Your Vacation</button>
-          </Link>
-        </form>
+        <div className="landingStart">
+          <div className="landingStart2">
+            <h1>My Travel Guide</h1>
+            <h2>Plan your dream trip today!</h2>
+            <form>
+              <input
+                onChange={this.handleChange}
+                type="text"
+                name="city"
+                placeholder="_______City_______"
+              />
+              <input
+                onChange={this.handleChange}
+                type="text"
+                name="country"
+                placeholder="______Country_____"
+              />
+              <Link to={`/home/${this.state.country}/${this.state.city}`}>
+                <button>Start Your Vacation</button>
+              </Link>
+            </form>
+          </div>
+        </div>
+        <div className="landingMiddle">
+          <h1>Your one-stop-shop </h1>
+          <h1 id="h1Sec">for a perfect trip</h1>
+          <div className="landingMiddle2">
+            <div className="landingText">
+              <p>
+                Fully customizable multi-destination trip planner with popular
+                itineraries to help you get started
+              </p>
+              <img src={require("../Images/gps.png")} alt="gps" />
+            </div>
+            <div className="landingText">
+              <p>
+                The hotel search with an extensive price comparison. The prices
+                shown come from numerous hotels and booking websites
+              </p>
+              <img src={require("../Images/hotel.png")} alt="hotel" />
+            </div>
+            <div className="landingText">
+              <p>
+                The best flight deals near you to all the best vacation
+                destinations
+              </p>
+              <img src={require("../Images/plane.png")} alt="plane" />
+            </div>
+            <div className="landingText">
+              <p>
+                Finding cool local hangouts and events as an undercover tourist
+                can be difficult, right? No worries, let us take care of it!
+              </p>
+              <img
+                src="https://cdn.onlinewebfonts.com/svg/img_120584.png"
+                alt="vip"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
